c3b7ba93957b72a6fe15ceb5f03f5b3ba32afa5a
ChenTJ004 <enzochen38@gmail.com>
2023-12-11 09:57:59 +0100

    Lab 19 completata

--- Lab19_AzzeraNonUnici/src-gen/javamm/AzzeraNonUnici.java
+++ Lab19_AzzeraNonUnici/src-gen/javamm/AzzeraNonUnici.java
@@ -6,36 +6,57 @@ public class AzzeraNonUnici {
     int m = M.length;
     int n = M[0].length;
     int elemento = M[r][c];
-    for (int i = 1; (i < n); i++) {
-      boolean _tripleEquals = (elemento == M[r][(c + i)]);
-      if (_tripleEquals) {
-        M[r][(c + i)] = 0;
+    int i = 1;
+    boolean nonUnico = false;
+    while (((c + i) < n)) {
+      {
+        boolean _tripleEquals = (elemento == M[r][(c + i)]);
+        if (_tripleEquals) {
+          M[r][(c + i)] = 0;
+          nonUnico = true;
+        }
+        i++;
       }
     }
-    for (int i = 1; (i < m); i++) {
+    i = 1;
+    while (((r + i) < m)) {
       {
         boolean _tripleEquals = (elemento == M[(r + i)][c]);
         if (_tripleEquals) {
           M[(r + i)][c] = 0;
+          nonUnico = true;
         }
-        boolean _tripleEquals_1 = (elemento == M[(r + i)][(c + i)]);
-        if (_tripleEquals_1) {
-          M[(r + i)][(c + i)] = 0;
-        }
+        i++;
       }
+    }
+    i = 1;
+    while ((((r + i) < m) && ((c + i) < n))) {
+      {
+        boolean _tripleEquals = (elemento == M[(r + i)][(c + i)]);
+        if (_tripleEquals) {
+          M[(r + i)][(c + i)] = 0;
+          nonUnico = true;
+        }
+        i++;
+      }
+    }
+    if (nonUnico) {
+      M[r][c] = 0;
     }
     return M;
   }
 
   public static int[][] azzeraNonUniciMatrice(int[][] M) {
-    return null;
+    int m = M.length;
+    int n = M[0].length;
+    for (int i = 0; (i < m); i++) {
+      for (int j = 0; (j < n); j++) {
+        AzzeraNonUnici.azzeraNonUnici(M, i, j);
+      }
+    }
+    return M;
   }
 
   public static void main(String[] args) {
-    int[][] M = new int[][] { new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4 } };
-    int r = 0;
-    int c = 1;
-    int[][] azzerata = new int[][] { new int[] { 1, 0, 3, 4 }, new int[] { 1, 0, 3, 4 }, new int[] { 1, 0, 3, 4 } };
-    System.out.println(AzzeraNonUnici.azzeraNonUnici(M, r, c));
   }
 }
