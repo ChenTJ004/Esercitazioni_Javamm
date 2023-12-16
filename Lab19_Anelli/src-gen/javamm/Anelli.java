ccc08adfa54a5d3fe80728bc889a9f2049cd4fa4
ChenTJ004 <enzochen38@gmail.com>
2023-12-16 14:38:09 +0100

    Caricati nuovi file

--- Lab19_Anelli/src-gen/javamm/Anelli.java
+++ Lab19_Anelli/src-gen/javamm/Anelli.java
@@ -5,13 +5,13 @@ public class Anelli {
   public static boolean anelloBilanciato(int[][] T, int k) {
     int m = T.length;
     int centro = (m / 2);
-    int somma = (T[(centro - k)][(centro - k)] + T[(centro + k)][(centro + k)]);
-    int somma2 = 0;
+    int elementoCentrale = T[centro][centro];
+    int somma = 0;
     for (int i = 0; (i < ((k * 2) + 1)); i++) {
       {
         int _plus = (T[(centro - k)][(centro + (i - k))] + T[(centro + k)][((centro + k) - i)]);
-        somma2 = _plus;
-        if ((somma != somma2)) {
+        somma = _plus;
+        if ((elementoCentrale != somma)) {
           return false;
         }
       }
@@ -19,8 +19,8 @@ public class Anelli {
     for (int i = 1; (i < (k * 2)); i++) {
       {
         int _plus = (T[(centro + (i - k))][(centro - k)] + T[((centro + k) - i)][(centro + k)]);
-        somma2 = _plus;
-        if ((somma != somma2)) {
+        somma = _plus;
+        if ((elementoCentrale != somma)) {
           return false;
         }
       }
